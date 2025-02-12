f865ac2daf04efb7e9bce584687949d6914c4bc5
PramodSeeram <seerampramodsailesh.com>
2025-02-12 21:59:36 +0530

    modified code by adding API EndPoints-transform,translation,rotation,scale

--- server.py
+++ server.py
@@ -8,5 +8,23 @@ def receive_transform():
     print("Received Transform Data:", data)
     return jsonify({"message": "Transform received successfully!"})
 
+@app.route('/translation', methods=['POST'])
+def receive_translation():
+    data = request.json
+    print("Received Translation Data:", {"name": data['name'], "location": data['location']})
+    return jsonify({"message": "Translation received successfully!"})
+
+@app.route('/rotation', methods=['POST'])
+def receive_rotation():
+    data = request.json
+    print("Received Rotation Data:", {"name": data['name'], "rotation": data['rotation']})
+    return jsonify({"message": "Rotation received successfully!"})
+
+@app.route('/scale', methods=['POST'])
+def receive_scale():
+    data = request.json
+    print("Received Scale Data:", {"name": data['name'], "scale": data['scale']})
+    return jsonify({"message": "Scale received successfully!"})
+
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
